c38961f9692be702419bd1a2d9c60cde8547c64f
Keiron-Beadle <K.BEADLE-2018@hull.ac.uk>
2021-10-05 15:32:52 +0100

    Add project files.

---
+++ Systems/OpenGLRenderer.cs
@@ -0,0 +1,88 @@
+﻿using OpenGL_Game.Components;
+using OpenGL_Game.Objects;
+using OpenGL_Game.OBJLoader;
+using OpenGL_Game.Scenes;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace OpenGL_Game.Systems
+{
+    class OpenGLRenderer : SystemRender
+    {
+        public OpenGLRenderer()
+        {
+            Name = "OpenGL Renderer";
+            InitialiseOpenGL();
+        }
+      
+        protected override void InitialiseOpenGL()
+        {
+            pgmID = GL.CreateProgram();
+            LoadShader("Shaders/vs.glsl", ShaderType.VertexShader, pgmID, out vsID);
+            LoadShader("Shaders/fs.glsl", ShaderType.FragmentShader, pgmID, out fsID);
+            GL.LinkProgram(pgmID);
+            Console.WriteLine(GL.GetProgramInfoLog(pgmID));
+
+            uniform_stex = GL.GetUniformLocation(pgmID, "s_texture");
+            uniform_mmodelviewproj = GL.GetUniformLocation(pgmID, "ModelViewProjMat");
+            uniform_mmodel = GL.GetUniformLocation(pgmID, "ModelMat");
+            uniform_diffuse = GL.GetUniformLocation(pgmID, "v_diffuse");     // OBJ NEW
+        }
+
+        void LoadShader(String filename, ShaderType type, int program, out int address)
+        {
+            address = GL.CreateShader(type);
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+            GL.AttachShader(program, address);
+            Console.WriteLine(GL.GetShaderInfoLog(address));
+        }
+
+        public override void OnAction(Entity entity)
+        {
+            if ((entity.Mask & MASK) == MASK)
+            {
+                List<IComponent> components = entity.Components;
+
+                IComponent geometryComponent = components.Find(delegate (IComponent component)
+                {
+                    return component.ComponentType == ComponentTypes.COMPONENT_GEOMETRY;
+                });
+                Geometry geometry = ((ComponentGeometry)geometryComponent).Geometry();
+
+                IComponent positionComponent = components.Find(delegate (IComponent component)
+                {
+                    return component.ComponentType == ComponentTypes.COMPONENT_POSITION;
+                });
+                Vector3 position = ((ComponentPosition)positionComponent).Position;
+                Matrix4 model = Matrix4.CreateTranslation(position);
+
+                Draw(model, geometry);
+            }
+        }
+
+        public override void Draw(Matrix4 model, Geometry geometry)
+        {
+            GL.UseProgram(pgmID);
+
+            GL.Uniform1(uniform_stex, 0);
+            GL.ActiveTexture(TextureUnit.Texture0);
+
+            GL.UniformMatrix4(uniform_mmodel, false, ref model);
+            Matrix4 modelViewProjection = model * GameScene.gameInstance.camera.view * GameScene.gameInstance.camera.projection;
+            GL.UniformMatrix4(uniform_mmodelviewproj, false, ref modelViewProjection);
+
+            geometry.Render(uniform_diffuse);   // OBJ CHANGED
+
+            GL.UseProgram(0);
+        }
+    }
+}
---
+++ Systems/SystemRender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenGL_Game.Components;
+using OpenGL_Game.OBJLoader;
+using OpenGL_Game.Objects;
+using OpenGL_Game.Scenes;
+
+namespace OpenGL_Game.Systems
+{
+    abstract class SystemRender : ISystem
+    {
+        protected const ComponentTypes MASK = (ComponentTypes.COMPONENT_POSITION | ComponentTypes.COMPONENT_GEOMETRY);
+
+        protected int pgmID;
+        protected int vsID;
+        protected int fsID;
+        protected int uniform_stex;
+        protected int uniform_mmodelviewproj;
+        protected int uniform_mmodel;
+        protected int uniform_diffuse;  // OBJ NEW
+        public string Name { get; protected set; }
+
+        public SystemRender()
+        {
+            Name = "System Render";
+        }
+
+        protected abstract void InitialiseOpenGL();
+        public abstract void OnAction(Entity entity);
+        public abstract void Draw(Matrix4 mat4, Geometry geom);
+    }
+}
